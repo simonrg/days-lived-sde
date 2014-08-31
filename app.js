e566bd40cabee3563b1ad2301862a70e1589fe5c
Simon <simon_rg@yahoo.com.au>
2014-08-31 15:46:32 +1000

    changes

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.post('/form', function(req, res){	//post to ../form url
 	//var days = Date.UTC(2012, dob.getMonth(), dob.getDate(), 0,0,0);
 	//var date = new Date();
 
-	res.send(user + ' ' + dob.toUTCString() + days);
 	//client.query("INSERT INTO birthdates(username) values($1)", [user]);
 	//client.query("INSERT INTO daysalive(name, dob) VALUES ($1, $2)", [user, dob]);
 });
