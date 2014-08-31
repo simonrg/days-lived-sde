cc040981138f731b5dc827db015089acec77b493
Simon <simon_rg@yahoo.com.au>
2014-08-31 16:08:12 +1000

    test

--- app.js
+++ app.js
@@ -13,22 +13,19 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/'));
 
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
 //handle data from the page form fields
 //app.use(express.bodyParser());
 
-// app.post('/form', function(req, res){	//post to ../form url
-// 	var user = req.body.name;
-// 	var dob = req.body.dob;
+app.post('/form', function(req, res){
+	//var user = req.body.name;
+	//var dob = req.body.dob;
 
-// 	//var days = Date.UTC(2012, dob.getMonth(), dob.getDate(), 0,0,0);
-// 	//var date = new Date();
+	//var days = Date.UTC(2012, dob.getMonth(), dob.getDate(), 0,0,0);
+	//var date = new Date();
 
-// 	//client.query("INSERT INTO birthdates(username) values($1)", [user]);
-// 	//client.query("INSERT INTO daysalive(name, dob) VALUES ($1, $2)", [user, dob]);
-// });
+	//client.query("INSERT INTO birthdates(username) values($1)", [user]);
+	//client.query("INSERT INTO daysalive(name, dob) VALUES ($1, $2)", [user, dob]);
+});
 
 
 
