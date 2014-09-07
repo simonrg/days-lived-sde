0b4d000a1734016f36439a510ce770c371684648
Simon <simon_rg@yahoo.com.au>
2014-09-07 21:07:59 +1000

    database config

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var pg = require('pg');
-var connstring = process.env.DATABASE_URL || 'pg://postgres:potatogate369@localhost:5432/mydb' //'postgres://localhost:5432/mydb'	
+var connstring = process.env.DATABASE_URL || 'postgres://localhost:5432/mydb'		//'pg://postgres:potatogate369@localhost:5432/mydb'
 var client = new pg.Client(connstring);
 client.connect();
 
@@ -17,8 +17,8 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
 });
 
 //serve the static html page
-// app.set('port', (process.env.PORT || 5000));
-// app.use(express.static(__dirname + '/'));
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/'));
 
 
 //handle data from the page form fields
