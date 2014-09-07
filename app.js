fabf7382d67d51457df8a30e43fd98cefb4e24ca
Simon <simon_rg@yahoo.com.au>
2014-09-07 20:31:34 +1000

    database

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 
-//var pg = require('pg');
-//var connstring = 'pg://postgres:potatogate369@localhost:5432/mydb'
-//var client = new pg.Client(connstring);
-//client.connect();
+var pg = require('pg');
+var connstring = process.env.DATABASE_URL || 'postgres://localhost:5432/dailyjs'	//'pg://postgres:potatogate369@localhost:5432/mydb'
+var client = new pg.Client(connstring);
+client.connect();
 
-//client.query("CREATE TABLE IF NOT EXISTS daysalive(name varchar(64), dob date, days smallint, submitted timestamp)");
+client.query("CREATE TABLE IF NOT EXISTS daysalive(name varchar(64), dob date, days smallint, submitted timestamp)");
 
 //serve the static html page
 app.set('port', (process.env.PORT || 5000));
@@ -27,8 +27,7 @@ app.post('/form', function(req, res){
 	var oneDay = 24*60*60*1000;
 	var coutDays = Math.round(Math.abs((date1.getTime() - date2.getTime())/(oneDay)));
 
-	res.send('success');
-	//client.query("INSERT INTO daysalive(name, dob, days, submitted) VALUES ($1, $2, $3, $4)", [user, dob, coutDays, date2]);
+	client.query("INSERT INTO daysalive(name, dob, days, submitted) VALUES ($1, $2, $3, $4)", [user, dob, coutDays, date2]);
 });
 
 
