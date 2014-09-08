92963b6c66abdfb44c1ef9daf248631f260f555b
Simon <simon_rg@yahoo.com.au>
2014-09-08 19:39:18 +1000

    insert

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.use(express.static(__dirname + '/'));
 
 
 app.use(express.bodyParser());
+
+
 //get stuff from the table
 app.post('/db', function (request, response) {
   
@@ -20,27 +22,15 @@ app.post('/db', function (request, response) {
 	var dob = request.body.dob;
 
 	//calculate days between dob and today
-	var mdy = dob.split('/');
+	var mdy = dob.split('-');
 	var date1 = new Date(mdy[2], mdy[1], mdy[0]);
 	var date2 = new Date();
 	var oneDay = 24*60*60*1000;
 	var coutDays = Math.round(Math.abs((date1.getTime() - date2.getTime())/(oneDay)));
 
-	// pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-	// 	//client.query("INSERT INTO daysalive(name, dob, days, submitted) VALUES ($1, $2, $3, $4)", [user, dob, coutDays, date2], function(err, result){
-	// 	client.query("INSERT INTO daysalive(name) VALUES ($1)", [user], function(err, result){
-	// 		done();
-	// 		if(err)
-	// 		{
-	// 			console.error(err);
-	// 			response.send("Error " + err);
-	// 		}
-	// 	});
-	// });
-
   	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
   		//client.query("INSERT INTO daysalive(name) VALUES ($1, $2, $3, $4)", [user, dob, coutDays, date2], function(err, result){ done(); });
-  		client.query("INSERT INTO daysalive(name, days, submitted) VALUES ($1, $2, $3)", [user, coutDays, date2], function(err, result)
+  		client.query("INSERT INTO daysalive(name, dob, days, submitted) VALUES ($1, $2, $3, $4)", [user, dob, coutDays, date2], function(err, result)
 		{ 
 			done(); 
 			if(err)
