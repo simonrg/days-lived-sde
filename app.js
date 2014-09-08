4d70796c48d795b2f435288b29d3531c43b30b10
Simon <simon_rg@yahoo.com.au>
2014-09-08 18:55:47 +1000

    db

--- app.js
+++ app.js
@@ -29,31 +29,31 @@ app.post('/db', function (request, response) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 		client.query("INSERT INTO daysalive(name, dob, days, submitted) VALUES ($1, $2, $3, $4)", [user, dob, coutDays, date2], function(err, result){
 		done();
-		if(err)
-		{
-			console.error(err);
-			response.send("Error " + err);
-		}
-		else
-		{
-			response.send(result.rows);
-		}
+			if(err)
+			{
+				console.error(err);
+				response.send("Error " + err);
+			}
+			else
+			{
+				response.send(result.rows);
+			}
 		});
 	});
 
-  	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    	client.query('SELECT * FROM daysalive', function(err, result) {
-      	done();
-      	if (err)
-       	{ 
-       		console.error(err); response.send("Error " + err); 
-       	}
-      	else
-       	{ 
-       		response.send(result.rows); 
-       	}
-    	});
-  	});
+  	// pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+   //  	client.query('SELECT * FROM daysalive', function(err, result) {
+   //    	done();
+   //    	if (err)
+   //     	{ 
+   //     		console.error(err); response.send("Error " + err); 
+   //     	}
+   //    	else
+   //     	{ 
+   //     		response.send(result.rows); 
+   //     	}
+   //  	});
+  	// });
 });
 
 
