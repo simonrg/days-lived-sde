97a1b5ff129e12765deafd36b50d19cb00c920a0
Simon <simon_rg@yahoo.com.au>
2014-09-08 18:27:03 +1000

    get

--- app.js
+++ app.js
@@ -15,6 +15,21 @@ app.use(express.static(__dirname + '/'));
 
 //get stuff from the table
 app.get('/db', function (request, response) {
+  
+	var user = req.body.name;
+	var dob = req.body.dob;
+
+	//calculate days between dob and today
+	var mdy = dob.split('/');
+	var date1 = new Date(mdy[2], mdy[1], mdy[0]);
+	var date2 = new Date();
+	var oneDay = 24*60*60*1000;
+	var coutDays = Math.round(Math.abs((date1.getTime() - date2.getTime())/(oneDay)));
+
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		client.query("INSERT INTO daysalive(name, dob, days, submitted) VALUES ($1, $2, $3, $4)", [user, dob, coutDays, date2]);
+	});
+
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM daysalive', function(err, result) {
       done();
