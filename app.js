2615265d9b920c04cbe84a69bcfd79e79d83a728
Simon <simon_rg@yahoo.com.au>
2014-09-08 20:25:42 +1000

    insert

--- app.js
+++ app.js
@@ -29,8 +29,7 @@ app.post('/db', function (request, response) {
 	var numdays = Math.round(Math.abs((date1.getTime() - timestamp.getTime())/(oneDay)));
 
   	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  		//client.query("INSERT INTO daysalive(name) VALUES ($1, $2, $3, $4)", [user, dob, coutDays, date2], function(err, result){ done(); });
-  		client.query("INSERT INTO daysalive(name, days, submitted) VALUES ($1, $2, $3)", [user, numdays, timestamp], function(err, result)
+  		client.query("INSERT INTO daysalive(name, dob, days, submitted) VALUES ($1, $2, $3, $4)", [user, dob, numdays, timestamp], function(err, result)
 		{ 
 			done(); 
 			if(err)
