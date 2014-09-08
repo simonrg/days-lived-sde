c1eef95dd839808c174c94ac04f547cca128e8fa
Simon <simon_rg@yahoo.com.au>
2014-09-08 19:20:58 +1000

    insert

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.post('/db', function (request, response) {
 	// });
 
   	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  		client.query("INSERT INTO daysalive(name) VALUES ($1)", [user], function(err, result){ done(); });
+  		client.query("INSERT INTO daysalive(name) VALUES ($1, $2, $3, $4)", [user, dob, coutDays, date2], function(err, result){ done(); });
     	client.query('SELECT * FROM daysalive', function(err, result) {
 	      	done();
 	      	if (err)
