b70f31d679c2c7eca4732e364be3b352f8d59e6a
Simon <simon_rg@yahoo.com.au>
2014-09-08 19:04:54 +1000

    db

--- app.js
+++ app.js
@@ -35,26 +35,26 @@ app.post('/db', function (request, response) {
 				console.error(err);
 				response.send("Error " + err);
 			}
-			else
-			{
-				response.send(result.rows);
-			}
+			// else
+			// {
+			// 	response.send(result.rows);
+			// }
 		});
 	});
 
-  	// pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-   //  	client.query('SELECT * FROM daysalive', function(err, result) {
-   //    	done();
-   //    	if (err)
-   //     	{ 
-   //     		console.error(err); response.send("Error " + err); 
-   //     	}
-   //    	else
-   //     	{ 
-   //     		response.send(result.rows); 
-   //     	}
-   //  	});
-  	// });
+  	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    	client.query('SELECT * FROM daysalive', function(err, result) {
+      	done();
+      	if (err)
+       	{ 
+       		console.error(err); response.send("Error " + err); 
+       	}
+      	else
+       	{ 
+       		response.send(result.rows); 
+       	}
+    	});
+  	});
 });
 
 
